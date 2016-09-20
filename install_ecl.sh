7b2871a48209374512338871b542688c4211ae59
s1dhart <sid@olmgroup.com>
2016-09-20 20:35:14 +0100

    Create install_ecl.sh

---
+++ install_ecl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/sid
+sudo java \
+-DTRACE=true \
+-DDEBUG=true \
+-Dinput.target.db.server.hostname=DBServer \
+-Dinput.target.db.server.dbname_schema=live \
+-Dinput.target.db.server.port=1999 \
+-Dinput.target.install.environment=live \
+-Dinput.target.db.username=postgres \
+-Dinput.target.db.password=postgres \
+-Dinput.target.db.platform=POSTGRESQL \
+-Dinput.target.install.user=autouser \
+-Dinput.target.install.group=autogroup \
+-jar /home/sid/eclipse-installer-1.3.0-SNAPSHOT.jar \
+-console \
+-options-system
+sudo start olm_live_eclipse
