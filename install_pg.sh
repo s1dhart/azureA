267a40fbbf0dc23489667366fc051be834f32d55
s1dhart <sid@olmgroup.com>
2016-09-25 22:25:41 +0100

    Update install_pg.sh

--- install_pg.sh
+++ install_pg.sh
@@ -9,26 +9,20 @@ sudo locale-gen en_GB.utf8
 ##
 #create postgres user for postgresql
 ##
-sudo ln -s /opt/postgresql-9.3.5 /opt/pgsql
 sudo mkdir -p /opt/pgsql_data
 sudo useradd -m postgres
 sudo chown -R postgres.postgres /opt/pgsql_data
+sudo chown -R postgres.postgres /opt/PostgreSQL
 ##
 # Set Environments
 ##
 sudo touch /home/postgres/.bash_profile
 sudo chown postgres.postgres /home/postgres/.bash_profile
-cat >> /tmp/postgres.sh <<EOF
 cat >> /home/postgres/.bash_profile <<EOFF
-export PGHOME=/opt/PostgreSQL
+export PGHOME=/opt/PostgreSQL/9.5
 export PATH=\\\$PATH:\\\$PGHOME/bin
 export MANPATH=\\\$MANPATH:\\\$PGHOME/share/man
-export DATA=\`date +"%Y%m%d%H%M"\`
-export PGUSER=postgres
-alias rm='rm -i'
-alias ll='ls -lh'
 EOFF
-source /home/postgres/.bash_profile
 # Install PG
 ./postgresql-9.5.4-1-linux-x64.run --mode unattended --prefix /opt/PostgreSQL/9.5 --datadir /opt/PostgreSQL/9.5/data --superpassword password --serverport 5432 --locale en_GB.utf8
 # Create DB
