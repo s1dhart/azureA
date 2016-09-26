76f02fe9492b1624779a50baf6ad816de1ee328c
s1dhart <sid@olmgroup.com>
2016-09-26 13:28:09 +0100

    Update install_pg.sh

--- install_pg.sh
+++ install_pg.sh
@@ -30,5 +30,5 @@ EOFF
 # Create DB
 sudo su - postgres -c "echo \"CREATE ROLE ecl_test LOGIN PASSWORD 'ecl_test' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';\" | PGPASSWORD=password psql -U postgres"
 sudo su - postgres -c "echo \"CREATE DATABASE ecl_test OWNER ecl_test ENCODING 'UTF8' LC_COLLATE 'en_GB.utf8' LC_CTYPE 'en_GB.utf8';\" | PGPASSWORD=password psql -U postgres"
-sudo printf "\nhost    all             all             10.0.2.40/24            trust\n" >> /opt/PostgreSQL/9.5/pg_hba.conf
+sudo printf "\nhost    all             all             10.0.2.40/24            trust\n" >> /opt/PostgreSQL/9.5/data/pg_hba.conf
 sudo service postgresql-9.5 restart
