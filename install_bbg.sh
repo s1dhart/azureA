cea6ac450d886fd04daf25626473faf20c65dc17
s1dhart <sid@olmgroup.com>
2016-09-26 15:15:48 +0100

    Update install_bbg.sh

--- install_bbg.sh
+++ install_bbg.sh
@@ -9,3 +9,11 @@ sudo tar Pxvzf nginx-conf.tar.gz
 sudo openssl dhparam -out /etc/nginx/ssl/dhparams.pem 2048 
 sudo ./bbgadmin.sh testpass 
 sudo service nginx start 
+sudo cp /etc/nginx/sites-available/eclipse_template /etc/nginx/sites-available/$1
+cd /etc/nginx/sites-available
+sudo sed -i s/443/$ssl/g $1
+sudo sed -i s/8011/$ajp/g $1
+sudo sed -i s/sidenv1/$1/g $1
+cd /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/$1 $1
+sudo service nginx reload
