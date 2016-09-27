5215b93392209916a3c930ba5e7daaeeabafeb8e
s1dhart <sid@olmgroup.com>
2016-09-27 11:37:45 +0100

    Update install_bbg.sh

--- install_bbg.sh
+++ install_bbg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 wget https://support.olmsystems.com/olm/application_store/downloads/BBG/V1_0.tar.gz 
+sudo apt-get update
 tar xvzf V1_0.tar.gz 
-sudo dpkg --install nginx-common.deb nginx-full.deb webmin.deb -f
+sudo dpkg --install --force-all nginx-common.deb nginx-full.deb webmin.deb
 sudo apt-get install -f 
-sudo /usr/share/webmin/install-module.pl webmin_nginx.gz -f
+sudo /usr/share/webmin/install-module.pl webmin_nginx.gz
 sudo service nginx stop 
 sudo tar Pxvzf nginx-conf.tar.gz
 sudo openssl dhparam -out /etc/nginx/ssl/dhparams.pem 2048 
